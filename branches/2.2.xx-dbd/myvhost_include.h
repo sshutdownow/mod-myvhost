35577e3313ec89aebd2b3cc35c64100422ad2d1e
iPopovi <iPopovi@4bc85d46-f536-0410-bce9-01c9dc5a8ffe>
2009-10-05 09:46:35 +0000

    fix for LDFLAGS parameters in Makefile, now it compiles on CentOS without any problems

---
+++ branches/2.2.xx-dbd/myvhost_include.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2005-2006 Igor Popov <igorpopov@newmail.ru>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * $Id$
+ *
+ */
+
+#ifndef __MYVHOST_INCLUDE_H__
+#define __MYVHOST_INCLUDE_H__
+
+#include <sys/types.h>
+#include <sys/cdefs.h>
+
+#include "ap_config.h"
+#include "httpd.h"
+#include "http_request.h"
+#include "http_config.h"
+#include "http_core.h"
+#include "http_log.h"
+#include "http_main.h"
+#include "http_protocol.h"
+#include "util_script.h"
+
+#include "apr.h"
+#include "apr_pools.h"
+#include "apr_strings.h"
+#include "apr_hash.h"
+#include "apr_time.h"
+#include "apr_dbd.h"
+#include "mod_dbd.h"
+
+
+#if !defined(__unused)
+
+#if defined(__GNUC__) && (__GNUC__ > 2 || __GNUC__ == 2 && __GNUC_MINOR__ >= 7)
+#define __unused __attribute__((__unused__))
+#else
+#define __unused
+#endif
+
+#endif /* __unused */
+
+
+#endif /* __MYVHOST_INCLUDE_H__ */
