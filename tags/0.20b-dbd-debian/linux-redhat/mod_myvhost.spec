06bfdb044a6ca2ab58c652a72357e8b73a1555f1
iPopovi <iPopovi@4bc85d46-f536-0410-bce9-01c9dc5a8ffe>
2010-11-05 10:46:23 +0000

    added tag 0.20b for ubuntu(debian) build

--- tags/0.20b-dbd-debian/linux-redhat/mod_myvhost.spec
+++ tags/0.20b-dbd-debian/linux-redhat/mod_myvhost.spec
@@ -3,8 +3,8 @@
 #
 Summary: Apache module for dynamically configured name-based mass virtual hosting with PHP.
 Name: mod_myvhost
-Version: 0.16
-Release: 3
+Version: 0.20
+Release: 9
 License: Apache-2.0
 URL: http://code.google.com/p/mod-myvhost/
 Group: System Environment/Daemons
@@ -12,7 +12,7 @@ Source: http://mod-myvhost.googlecode.com/files/%{name}-%{version}.tar.gz
 #Source1: 
 BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
 Requires: httpd httpd-mmn = %([ -a %{_includedir}/httpd/.mmn ] && cat %{_includedir}/httpd/.mmn || echo missing)
-BuildRequires: httpd-devel mysql-devel
+BuildRequires: httpd-devel
 
 %description
 mod_myvhost is Apache module for dynamically configured name based mass virtual
@@ -51,22 +51,28 @@ rm -rf %{buildroot}
 %attr(0644,root,root) %config(noreplace) %{_sysconfdir}/httpd/conf.d/zz%{name}.conf
 
 %changelog
-* Thu Feb 18 2010 Igor Popov <ipopovi@gmail.com>
+* Fri Mar 19 2010 Igor Popov <ipopovi@gmail.com> - v0.20b
+- set upload_tmp_dir to $DocRoot/.tmp if exists
+- DBD version seems to work, but missed some functionality.
+- DBD verion is heavily based on mod_vhost_dbd http://code.google.com/p/dbd-modules
+- DBD at least version 1.3.x of APR-Utils libraries is required.
+
+* Thu Feb 18 2010 Igor Popov <ipopovi@gmail.com> - v0.16
 - vhosts that are defined in apache config and created on fly works together
 
-* Tue Feb 16 2010 Igor Popov <ipopovi@gmail.com>
+* Tue Feb 16 2010 Igor Popov <ipopovi@gmail.com> - v0.15-2
 - beta version of the mod_myvhost for Apache 2.xx branch
 - RPM spec for Apache 2.0.xx branch
 - workaround for apr_hash_clear
 
-* Tue Dec 05 2006 Igor Popov <ipopovi@gmail.com>
+* Tue Dec 05 2006 Igor Popov <ipopovi@gmail.com> - v0.15
 - Just changed from using Apache's ap_psprintf to libc's snprintf,
   it allows to use more complicated directives like %1$s and
   in turn it allows very simple solvation of problem finding vhost
   by name or alias, for example:
     SELECT `rootdir` FROM `db` WHERE (`vname`='%1$s' OR `valias`='%1$s');
 - Also added expiremental feature - set uid and gid for suexec.
-                            
+
 * Sat Jun 03 2006 Igor Popov <ipopovi@gmail.com>
 - Added internal implementation of mysql_escape_string().
 - Some minor fixes.
---
+++ tags/0.20b-dbd-debian/mod_myvhost_cache.c
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2005-2010 Igor Popov <ipopovi@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+
+#ifdef WITH_CACHE
+
+#include "myvhost_include.h"
+#include "mod_myvhost.h"
+#include "mod_myvhost_cache.h"
+
+static const char __unused cvsid[] = "$Id$";
+
+p_cache_t cache_vhost_find(myvhost_cfg_t *cfg, const char *hostname)
+{
+    p_cache_t vhost;
+    apr_time_t cur;
+    
+    if (!cfg->cache_enabled) {
+	return NULL;
+    }
+
+    vhost = apr_hash_get(cfg->cache, hostname, APR_HASH_KEY_STRING);
+    if (!vhost) {
+	return NULL;
+    }
+
+    cur = apr_time_now();
+
+    if (vhost->hits > 0 && vhost->hits < 512 && vhost->access_time + 300 >= cur) {
+	vhost->hits++;
+    } else if (vhost->hits < 0 && vhost->hits > -256 && vhost->access_time + 180 >= cur) {
+	vhost->hits--;
+    } else {
+	apr_hash_set(cfg->cache, hostname, APR_HASH_KEY_STRING, NULL);	/* delete hash entry */
+	vhost = NULL;
+    }
+    return vhost;
+}
+
+void cache_vhost_add(myvhost_cfg_t *cfg,
+		     const char *hostname,
+		     const char *root,
+		     const char *admin,
+#ifdef WITH_PHP
+		     const char *php_ini_conf,
+#endif
+#ifdef WITH_UID_GID
+		     const int uid, const int gid,
+#endif
+		     const int hits)
+{
+    p_cache_t vhost;
+
+    if (!cfg->cache_enabled) {
+	return;
+    }
+
+    vhost = apr_pcalloc(cfg->pool, sizeof(cache_t));
+    vhost->access_time = apr_time_now();
+    vhost->root = apr_pstrdup(cfg->pool, root);
+    vhost->admin = apr_pstrdup(cfg->pool, admin);
+#ifdef WITH_PHP
+    vhost->php_ini_conf = apr_pstrdup(cfg->pool, php_ini_conf);
+#endif
+    vhost->hits = hits;
+#ifdef WITH_UID_GID
+    vhost->uid = uid;
+    vhost->gid = gid;
+#endif
+    apr_hash_set(cfg->cache, hostname, APR_HASH_KEY_STRING, vhost);
+}
+
+void cache_vhost_del(myvhost_cfg_t *cfg, apr_hash_t *cache, const char *host)
+{
+    if (!cfg->cache_enabled) {
+	return;
+    }
+    apr_hash_set(cache, host, APR_HASH_KEY_STRING, NULL);	/* delete hash entry */
+}
+
+
+/*  
+ *    apr_hash_clear appeared in apr 1.3.0
+ */
+#if !defined(APR_VERSION_AT_LEAST) 
+struct apr_hash_entry_t {
+    struct apr_hash_entry_t *next;
+    unsigned int      hash;
+    const void       *key;
+    apr_ssize_t       klen;
+    const void       *val;
+};
+		    	    	    	    
+struct apr_hash_index_t {
+    apr_hash_t         *ht;
+    struct apr_hash_entry_t   *this, *next;
+    unsigned int        index;
+};
+
+    apr_hash_index_t *hi;
+#endif
+
+/* FIXME: delete entries that is really older */
+void cache_vhost_flush(myvhost_cfg_t *cfg, apr_hash_t *cache, time_t older __unused)
+{
+#if !defined(APR_VERSION_AT_LEAST) 
+    apr_hash_index_t *hi;
+#endif
+
+    if (!cfg->cache_enabled) {
+	return;
+    }
+    if (!cache) {
+	return;
+    }
+               
+#if !defined(APR_VERSION_AT_LEAST)
+    for (hi = apr_hash_first(NULL, cache); hi; hi = apr_hash_next(hi))
+	apr_hash_set(cache, hi->this->key, hi->this->klen, NULL);
+#else
+    apr_hash_clear(cache);
+#endif
+}
+
+#endif /* WITH_CACHE */
