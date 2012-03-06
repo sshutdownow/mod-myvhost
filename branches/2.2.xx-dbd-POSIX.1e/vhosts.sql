f754c0c26f60c83242b30f99e0846623270b024d
iPopovi <iPopovi@4bc85d46-f536-0410-bce9-01c9dc5a8ffe>
2012-03-06 07:52:33 +0000

    begin work to add MPM-ITK support

---
+++ branches/2.2.xx-dbd-POSIX.1e/vhosts.sql
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2005-2010 Igor Popov <ipopovi@gmail.com>
+ *
+ * $Id$
+ *
+ */
+
+CREATE DATABASE `hosting`;
+USE `hosting`;
+
+CREATE TABLE `vhosts` (
+    `vhost`   varchar(255) NOT NULL default '',
+    `valias`  varchar(255) default '',
+    `enabled` enum('yes','no') NOT NULL default 'no',
+    `rootdir` varchar(255) NOT NULL default '',
+    `admin`   varchar(255) default '',
+    `extra_php_config` text,
+    UNIQUE KEY `vhostname` (`vhost`, `valias`),
+    KEY `enabled` (`enabled`)
+) TYPE=MyISAM COMMENT='vhosts';
+
+GRANT SELECT ON `hosting`.`vhosts` TO 'nonpriv'@'localhost' IDENTIFIED BY 'MeGaPassvv0d';
+
+INSERT INTO `vhosts` VALUES ('w3.myvhost1.net', NULL, 'yes', '/var/www/vhosts/01', 'www@vhost1.net', 'enable_dl=0;default_charset=koi8-r;');
+INSERT INTO `vhosts` VALUES ('www.myvhost2.net', 'myvhost2.net','yes', '/var/www/vhosts/02', 'www@vhost2.net', 'default_charset=koi8-r;');
+
+COMMIT;
